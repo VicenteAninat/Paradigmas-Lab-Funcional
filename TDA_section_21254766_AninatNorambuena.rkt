a37e909f0da7f547fd29350262752214791669c9
Vicente Aninat <vicente.aninat@usach.cl>
2024-04-09 23:12:22 -0400

    Se añadió documentación al TDA section

--- TDA_section_21254766_AninatNorambuena.rkt
+++ TDA_section_21254766_AninatNorambuena.rkt
@@ -1,12 +1,19 @@
 #lang racket
+(require "TDA_type_21254766_AninatNorambuena.rkt")
+(require "TDA_station_21254766_AninatNorambuena.rkt")
+(provide section)
+(provide get-distance-section)
+
 ;TDA section
+
 
 ;Constructor
 
-;Descripción:
-;Dominio:
-;Recorrido:
-;Recursión:
+;Descripción: Función que permite establecer enlaces entre dos estaciones.
+;Dominio: point1 (station)  X point2 (station) X distance (int positivo)
+;X cost (int positivo). 
+;Recorrido: section
+;Recursión: No aplica
 (define section
   (lambda (point1 point2 distance cost)
     (list point1 point2 distance cost)))
@@ -14,72 +21,72 @@
 
 ;Selectores
 
-;Descripción:
-;Dominio:
-;Recorrido:
-;Recursión:
+;Descripción: Extrae el point1 de una section
+;Dominio: section
+;Recorrido: station
+;Recursión: No aplica
 (define get-point1-section
-  (lambda section
+  (lambda (section)
     (car section)))
 
-;Descripción:
-;Dominio:
-;Recorrido:
-;Recursión:
+;Descripción: Extrae el point2 de una station
+;Dominio: section
+;Recorrido: station
+;Recursión: No aplica
 (define get-point2-section
-  (lambda section
+  (lambda (section)
     (car (cdr section))))
 
-;Descripción:
-;Dominio:
-;Recorrido:
-;Recursión:
+;Descripción: Extrae la distancia de una section
+;Dominio: section
+;Recorrido: int positivo
+;Recursión: No aplica
 (define get-distance-section
-  (lambda section
+  (lambda (section)
     (car (cddr section))))
 
-;Descripción:
-;Dominio:
-;Recorrido:
-;Recursión:
+;Descripción: Extrae el costo de una section
+;Dominio: section
+;Recorrido: int positivo
+;Recursión: No aplica
 (define get-cost-section
-  (lambda section
+  (lambda (section)
     (car (cdddr section))))
 
 
 ;Modificadores
 
-;Descripción:
-;Dominio:
-;Recorrido:
-;Recursión:
+;Descripción: Modifica el point1 de una section
+;Dominio: section X station
+;Recorrido: section 
+;Recursión: No aplica
 (define set-point1-section
   (lambda (section point1)
     (list point1 (get-point2-section section)
           (get-distance-section section) (get-cost-section section))))
 
-;Descripción:
-;Dominio:
-;Recorrido:
-;Recursión:
+;Descripción: Modifica el point2 de una section
+;Dominio: section X station
+;Recorrido: section
+;Recursión: No aplica
 (define set-point2-section
   (lambda (section point2)
     (list (get-point1-section section) point2
           (get-distance-section section) (get-cost-section section))))
 
-;Descripción:
-;Dominio:
-;Recorrido:
-;Recursión:
+;Descripción: Modifica la distania de una section
+;Dominio: section X int positivo
+;Recorrido: section
+;Recursión: No aplica
 (define set-distance-section
   (lambda (section distance)
     (list (get-point1-section section) (get-point2-section section)
           distance (get-cost-section section))))
 
-;Descripción:
-;Dominio:
-;Recorrido:
-;Recursión:
+;Descripción: Modifica el costo de una section
+;Dominio: section X int positivo
+;Recorrido: section
+;Recursión: No aplica
 (define set-cost-section
   (lambda (section cost)
     (list (get-point1-section section) (get-point2-section section)
