38bfdd24223bcc228a2bad0bc1e5b1977875a108
Vicente Aninat <vicente.aninat@usach.cl>
2024-04-10 22:33:31 -0400

    Se añadió documentación al TDA pcar

--- TDA_pcar_21254766_AninatNorambuena.rkt
+++ TDA_pcar_21254766_AninatNorambuena.rkt
@@ -5,11 +5,11 @@
 
 ;Constructor
 
-;Descripción:
-;Dominio:
-;Recorrido:
-;Recursión:
-
+;Descripción: Permite crear los carros de pasajeros que conforman un convoy.
+;Los carros pueden ser de tipo terminal (t) o central (c)
+;Dominio: id (int) X capacity (positive integer) X model (string) X type (car-type)
+;Recorrido: pcar
+;Recursión: No aplica
 (define pcar
   (lambda (id capacity model type)
     (list id capacity model type)))
@@ -17,38 +17,34 @@
 
 ;Selectores
 
-;Descripción:
-;Dominio:
-;Recorrido:
-;Recursión:
-
+;Descripción: Función que extrae el id de un pcar
+;Dominio: pcar
+;Recorrido: int
+;Recursión: No aplica
 (define get-id-pcar
   (lambda pcar
     (car pcar)))
 
-;Descripción:
-;Dominio:
-;Recorrido:
-;Recursión:
-
+;Descripción: Función que extrae la capacidad de un pcar
+;Dominio: pcar
+;Recorrido: int
+;Recursión: No aplica
 (define get-capacity-pcar
   (lambda pcar
     (car (cdr pcar))))
 
-;Descripción:
-;Dominio:
-;Recorrido:
-;Recursión:
-
+;Descripción: Función que extrae el modelo de un pcar
+;Dominio: pcar
+;Recorrido: string
+;Recursión: No aplica
 (define get-model-pcar
   (lambda pcar
     (car (cddr pcar))))
 
-;Descripción:
-;Dominio:
-;Recorrido:
-;Recursión:
-
+;Descripción: Función que extrae el tipo de carro de un pcar
+;Dominio: pcar
+;Recorrido: car-type
+;Recursión: No aplica
 (define get-type-pcar
   (lambda pcar
     (car (cdddr pcar))))
@@ -56,41 +52,37 @@
 
 ;Modificadores
 
-;Descripción:
-;Dominio:
-;Recorrido:
-;Recursión:
-
+;Descripción: Modifica el id de un pcar
+;Dominio: pcar X int
+;Recorrido: pcar
+;Recursión: No aplica
 (define set-id-pcar
   (lambda (pcar id)
     (list id (get-capacity-pcar pcar)
           (get-model-pcar pcar) (get-type-pcar pcar))))
 
-;Descripción:
-;Dominio:
-;Recorrido:
-;Recursión:
-
+;Descripción: Modifica la capacidad de un pcar
+;Dominio: pcar X int
+;Recorrido: pcar
+;Recursión: No aplica
 (define set-capacity-pcar
   (lambda (pcar capacity)
     (list (get-id-pcar pcar) capacity
           (get-model-pcar pcar) (get-type-pcar pcar))))
 
-;Descripción:
-;Dominio:
-;Recorrido:
-;Recursión:
-
+;Descripción: Modifica el modelo de un pcar
+;Dominio: pcar X string
+;Recorrido: pcar
+;Recursión: No aplica
 (define set-model-pcar
   (lambda (pcar model)
     (list (get-id-pcar pcar) (get-capacity-pcar pcar)
           model (get-type-pcar pcar))))
 
-;Descripción:
-;Dominio:
-;Recorrido:
-;Recursión:
-
+;Descripción: Modifica el tipo de carro de un pcar
+;Dominio: pcar X car-type
+;Recorrido: pcar
+;Recursión: No aplica
 (define set-type-pcar
   (lambda (pcar type)
     (list (get-id-pcar pcar) (get-capacity-pcar pcar)
