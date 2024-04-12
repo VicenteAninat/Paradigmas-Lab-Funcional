6a5763ffcf4e54e885ee7f75386b9629e1e4fcbf
Vicente Aninat <vicente.aninat@usach.cl>
2024-04-11 23:24:44 -0400

    Se modificó la implementación del TDA linea

--- TDA_line_21254766_AninatNorambuena.rkt
+++ TDA_line_21254766_AninatNorambuena.rkt
@@ -6,6 +6,9 @@
 (provide get-section-line)
 (provide get-id-line)
 (provide line-length)
+(provide line-section-length)
+(provide line-cost)
+(provide line-add-section)
 
 ;TDA line
 
@@ -86,43 +89,42 @@
           rail-type (get-section-line line))))
 
 ;-----------------------------------------------------------------------------------------------------
-;EN DESARROLLO
+;Descripción: Función interna que permite añadir tramos a una línea
+;Dominio: section* X section (section)
+;Recorrido: section*
+;Recursión: Natural
+(define añadir-section
+  (lambda (lista-section section)
+    (if (equal? lista-section null)
+        (cons section null)
+        (if (equal? (car lista-section) section)
+            (lista-section)
+            (cons (car lista-section) (cons (añadir-section (cdr lista-section) section) null))))))
 
 ;Descripción: Función que permite añadir tramos a una línea
-;Dominio:
-;Recorrido:
-;Recursión:
-
-
+;Dominio: line (line) X section (section)
+;Recorrido: line
+;Recursión: Natural
 (define line-add-section
   (lambda (line section)
-    (if ()
-        ()
-        (list (get-id-line line) (get-name-line line) (get-rail-type-line line) ()))))
-
-(define line-add-section
-  (lambda (line section)
-    (if (member? (section) (get-section-line line))
-        (list (get-id-line line) (extract-name-msg flow) (extract-option flow) )
-        (list (extract-id flow) (extract-name-msg flow) (append (extract-option flow) option)))))
-
-(define line-add-section
-  (lambda (line section)
-    (cond ()
-
-
-    (if (equal? (get-section-line line) null)
-        (section)
-        (if (equal? (car (get-section-line line)) section)
-            ()
-            (list (get-id-line line) (get-name-line line) (get-rail-type-line line) ()))))))
+    (list (get-id-line line) (get-name-line line) (get-rail-type-line line) (añadir-section (get-section-line line) section))))
             
 ;-----------------------------------------------------------------------------------------------------
 
 ;Pertenencia
 
 ;-----------------------------------------------------------------------------------------------------
-;
+;EN DESARROLLO
+;Descripción: Función que permite añadir tramos a una línea
+;Dominio: line (line) X section (section)
+;Recorrido: line
+;Recursión: Natural
+(define verificar-sections
+  (lambda (lista-sections)
+    (if (lista-sections)
+;Si la lista es nula dar true
+        ;sino proguntar por el primer elemento, si es section pasar al siguiente, sino falso
+       
 ;Descripción: Función que permite determinar si un elemento
 ;cumple con las restricciones señaladas en apartados
 ;anteriores en relación a las estaciones y tramos para
@@ -130,8 +132,14 @@
 ;Dominio: line
 ;Recorrido: bool
 ;Recursión:
-
-
+(define line?
+  (lambda (line)
+    (cond
+      ((not (integer? (get-id-line line))) #f)
+      ((not (string? (get-name-line line))) #f)
+      ((not (string? (get-rail-type-line line))) #f)
+      ((verificar-sections (get-section-line line)) #t))
+    ))
 
 ;Auxiliares
 
@@ -142,15 +150,79 @@
 ;Recursión: No aplica
 (define line-length
   (lambda (line)
-    (apply + (map get-distance-section(get-section-line line)))))    
+    (apply + (map get-distance-section(get-section-line line)))))
 
 ;-----------------------------------------------------------------------------------------------------
-;EN DESARROLLO
+;EN DESARROLLO   
+;Descripción: Función que permite determinar el largo de una lista de sections
+;Dominio: section*
+;Recorrido: int
+;Recursión: No aplica
+(define sacar-distancia
+  (lambda lista-sections
+    (apply + lista-sections)))
+
+;Descripción: Función que retorna lo que hay en medio de dos elementos de una lista
+;Dominio: Lista X Any X Any
+;Recorrido: Lista
+;Recursión: No aplica
+(define cortar-lista
+  (lambda (lista elem1 elem2)
+    (reverse (member elem2 (reverse (member elem1 lista))))))
+
+;Descripción: Función que retorna una estacion o vacio en base a su coincidencia con un nombre
+;Dominio: station X string
+;Recorrido: station
+;Recursión: No aplica
+(define comprobar-nombre
+  (lambda (estacion nombre)
+    (if (equal? (get-name-station estacion) nombre)
+        (estacion)
+        '())))
+
+;Descripción: Función que encuentra una estación de una lista de estaciones
+;Dominio: station*
+;Recorrido: station
+;Recursión: No aplica
+(define encontrar-estacion-1
+  (lambda (lista-estaciones nombre)
+    (car (map comprobar-nombre lista-estaciones nombre))))
+
+;Descripción: Función que retorna una section o vacio en base a su coincidencia con una estacion
+;Dominio: 
+;Recorrido: 
+;Recursión: No aplica
+(define comprobar-point1
+  (lambda (seccion estacion)
+    (if (equal? (get-point1-section seccion) estacion)
+        (seccion)
+        '())))
+
+(define comprobar-point2
+  (lambda (seccion estacion)
+    (if (equal? (get-point2-section seccion) estacion)
+        (seccion)
+        '())))
+
+;Descripción: Función que encuentra una seccion de una lista de secciones
+;Dominio: station*
+;Recorrido: station
+;Recursión: No aplica
+(define encontrar-estacion-2
+  (lambda (lista-estaciones nombre)
+    (car (map comprobar-nombre lista-estaciones nombre))))
+
 ;Descripción: Función que permite determinar el trayecto entre una estación origen y una final.
 ;Dominio:
 ;Recorrido:
 ;Recursión:
-
+(define line-section-length
+  (lambda (line nombre1 nombre2)
+    (sacar-distancia (cortar-lista (get-section-line line)
+                                   (map comprobar-point1 (get-section-line line)
+                                        (encontrar-estacion-1 (map get-point1-section (get-section-line line)) nombre1))
+                                   (map comprobar-point2 (get-section-line line)
+                                        (encontrar-estacion-1 (map get-point2-section (get-section-line line)) nombre2))))))
 
 ;-----------------------------------------------------------------------------------------------------
 
@@ -175,12 +247,12 @@
 
 ;-----------------------------------------------------------------------------------------------------
 ;EN DESARROLLO
-;Descripción: Función que permite determinar el costo de un trayecto entre una estación origen y una final.
-;Dominio:
-;Recorrido:
-;Recursión:
 
-(define line-section-cost
-  (lambda (line station1-name station2-name)))
+
+;Descripción: Función que permite determinar el costo de un trayecto entre una estación origen y una final.
+;Dominio: line
+;Recorrido: int
+;Recursión: De cola
+    
 
 ;-----------------------------------------------------------------------------------------------------
